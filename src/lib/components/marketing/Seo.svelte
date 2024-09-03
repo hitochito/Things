fa89a52c1b8bed7a3407d61dd0c44f5eb0679c1f
hitochito <hitochito0@gmail.com>
2024-09-04 05:55:41 +0600

    improve description

--- src/lib/components/marketing/Seo.svelte
+++ src/lib/components/marketing/Seo.svelte
@@ -2,9 +2,9 @@
 	import { images } from '$lib/assets';
 	import { CldOgImage } from 'svelte-cloudinary';
 
-	export let title = 'Things - Get Things Done: Your Ultimate Procrastination Tool';
+	export let title = 'Things - a To-Do app that lets you pretend you are getting things done.';
 	export let description =
-		'Discover the ultimate procrastination tool with Things, built using Svelte 5 and service workers. Features include a todo list with add, complete, and delete options, plus optional drag-and-drop reordering and offline functionality. Perfect for managing tasks you will never actually do!';
+		'Discover the ultimate procrastination tool built using Svelte 5 and service workers. As mentioned on Discord, this is a todo list with add, complete, and delete options, plus optional drag-and-drop reordering and offline functionality. Perfect for managing tasks you will never actually do!';
 	export let src = images.ogImageThings;
 	export let height = 630;
 	export let width = 1200;
